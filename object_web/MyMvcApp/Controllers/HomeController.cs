cf2155510c2f43b57d9c3525be15f109843c83a4
Robert Nester Odhiambo <odhiamborobert447@gmail.com>
2024-06-16 21:31:54 +0300

    about page

--- object_web/MyMvcApp/Controllers/HomeController.cs
+++ object_web/MyMvcApp/Controllers/HomeController.cs
@@ -1,31 +1,48 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using MyMvcApp.Models;
 
-namespace MyMvcApp.Controllers;
-
-public class HomeController : Controller
+namespace MyMvcApp.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        // This action method returns the "about_converted" view explicitly by name.
+        public IActionResult About()
+        {
+            return View("About_converted");
+        }
+
+        public IActionResult Login()
+        {
+            return View("login_converted");
+        }
+
+        public IActionResult Signup()
+        {
+            return View("signup_converted");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = HttpContext.TraceIdentifier });
+        }
     }
 }
